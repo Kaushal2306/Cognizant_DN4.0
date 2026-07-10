311524087a9dca6a46faede47dacf7a941b84f00
Kaushal Amara <amarakoushal@gmail.com>
2026-07-10 15:19:50 +0530

    SLF4J logging mandatory hands-on exercise

---
+++ week 1/SLF4LJ_Logging/src/main/java/org/example/slf4lj_logging/Slf4LjLoggingApplication.java
@@ -0,0 +1,13 @@
+package org.example.slf4lj_logging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Slf4LjLoggingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Slf4LjLoggingApplication.class, args);
+    }
+
+}
---
+++ week 1/SLF4LJ_Logging/src/test/java/org/example/slf4lj_logging/Slf4LjLoggingApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.slf4lj_logging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Slf4LjLoggingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
